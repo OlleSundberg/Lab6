40095282c51342b39a98edffd1199f9d1ec17499
Olle Sundberg <olle.sundbergg@gmail.com>
2017-10-31 12:58:00 +0100

    Add bouncer.cs

---
+++ Lab6/Lab6/Bouncer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class Bouncer
+    {
+        MainWindow mw;
+        public Bouncer(MainWindow mw)
+        {
+            this.mw = mw;
+
+            Task t1 = Task.Run(() =>
+            {
+                while (true)
+                {
+                    if (mw.Open)
+                    {
+                        Thread.Sleep(new Random().Next(mw.BouncerTimeMin, mw.BouncerTimeMax + 1) * 1000);
+                        if (mw.Open)
+                        {
+                            mw.BarQueue.Enqueue(new Patron(mw, mw.GetName()));
+                            mw.Dispatcher.Invoke(() => mw.Title += '!');
+                        }
+                    }
+                    else
+                        Thread.Sleep(1);
+                }
+            });
+        }
+    }
+}
